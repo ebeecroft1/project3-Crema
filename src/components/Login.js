ad11af2e6ba9f1d2f0e7d999053f0d1619a1f2ac
ebeecroft1 <eric.beecroft@gmail.com>
2022-05-23 12:45:01 +1000

    google sign in working

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,36 @@
 import { useState } from "react";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
-import { auth } from "../firebase-config"
+import { auth } from "../firebase-config";
+import GoogleButton from 'react-google-button';
 
 function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const navigate = useNavigate();
+
+    const provider = new GoogleAuthProvider();
+    const signInWithGoogle = () => {
+        signInWithPopup(auth, provider)
+        .then((result) => {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken;
+            // The signed-in user info.
+            const user = result.user;
+            // ...
+            navigate("/");
+        }).catch((error) => {
+            // Handle Errors here.
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // The email of the user's account used.
+            const email = error.customData.email;
+            // The AuthCredential type that was used.
+            const credential = GoogleAuthProvider.credentialFromError(error);
+            // ...
+        });
+    }
 
     const _handleSubmit = (event) => {
         event.preventDefault();
@@ -38,6 +62,10 @@ function Login() {
                 />
                 <button>Login</button>
             </form>
+            <GoogleButton
+                onClick={signInWithGoogle}
+                content="Sign in with google"
+            />
         </div>
     );
 
