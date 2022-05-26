994f39196439abce95cd5f688f08ec4f06a88504
ebeecroft1 <eric.beecroft@gmail.com>
2022-05-26 16:57:37 +1000

    successfully deployed via firebase

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,8 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import AuthProvider from "./AuthProvider";
-import Cafes from "./cafes/Cafes";
-import Login from "./users/Login";
-import Signup from "./users/Signup";
 import Navigation from "./Navigation";
-import Profile from "./users/Profile";
-import Home from "./Home";
-import Map from "./map/Map";
+import Pages from "./Pages";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.scss";
 
@@ -16,19 +11,10 @@ function App() {
 
   return (
     <AuthProvider>
-      <div className="App">
         <BrowserRouter>
           <Navigation />
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/cafes" element={<Cafes />} />
-            <Route exact path="/login" element={<Login />} />
-            <Route exact path="/signup" element={<Signup />} />
-            <Route exact path="/profile" element={<Profile />} />
-            <Route exact path="/map" element={<Map />} />
-          </Routes>
+          <Pages />
         </BrowserRouter>
-      </div>
     </AuthProvider>
   );
 }
---
+++ src/components/Pages.js
@@ -0,0 +1,28 @@
+import React from "react";
+import {Route, Routes, useLocation } from "react-router-dom";
+import Profile from "./users/Profile";
+import Home from "./Home";
+import Map from "./map/Map";
+import Cafes from "./cafes/Cafes";
+import Login from "./users/Login";
+import Signup from "./users/Signup";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.scss";
+
+function Pages() {
+    const location = useLocation();
+    return (
+        <div className="App">
+        <Routes location={location} key={location.pathname}>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/cafes" element={<Cafes />} />
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/signup" element={<Signup />} />
+            <Route exact path="/profile" element={<Profile />} />
+            <Route exact path="/map" element={<Map />} />
+        </Routes>
+        </div>
+    );
+}
+
+export default Pages;
